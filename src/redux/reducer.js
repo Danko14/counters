68ae21685a66c27362e13374ae7f2f6e6dae5b01
Ivan Nekrasov <ivan@Air-Ivan.(none)>
2022-07-02 23:14:37 +0300

    state immatability fix

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,21 +3,25 @@ const initialState = { maxId: 0, counters: [{ id: 0, value: 0 }] }
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INC': {
-            const idx = state.counters.findIndex((x) => x.id == action.payload)
-            if (~idx) {
-                state.counters[idx].value += 1
-                return {
-                    ...state,
-                }
-            } else {
-                return { ...state }
+            return {
+                ...state,
+                counters: state.counters.map((counter) => {
+                    if (counter.id == action.payload) {
+                        return { ...counter, value: counter.value + 1 }
+                    }
+                    return counter
+                }),
             }
         }
         case 'DEC': {
-            const counter = state.counters.find((x) => x.id == action.payload)
-            counter.value = counter.value - 1
             return {
                 ...state,
+                counters: state.counters.map((counter) => {
+                    if (counter.id == action.payload) {
+                        return { ...counter, value: counter.value - 1 }
+                    }
+                    return counter
+                }),
             }
         }
         case 'ADD':
